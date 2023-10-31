316fb94fdda0680b210921d977cb61721622ee62
Eli Lemons <me@elilemons.com>
2023-10-31 17:13:19 -0400

    Successful sign up page

---
+++ src/views/Unauthenticated/SignupSuccess/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const SignupSuccess: React.FC = () => {
+  // TODO Remove this test code
+  console.log('ELITEST SignupSuccess')
+  // ^ TODO Remove this test code
+
+  return (
+    <div>
+      You have successfully signed up. Once you confirm your email you will be able to sign in.
+    </div>
+  )
+}
+
+export default SignupSuccess
