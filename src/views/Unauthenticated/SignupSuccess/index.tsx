706d85f174b611c31076f5d61f8ec28cafb3573d
Eli Lemons <me@elilemons.com>
2023-10-31 17:13:24 -0400

    Rem log

--- src/views/Unauthenticated/SignupSuccess/index.tsx
+++ src/views/Unauthenticated/SignupSuccess/index.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react'
 
 const SignupSuccess: React.FC = () => {
-  // TODO Remove this test code
-  console.log('ELITEST SignupSuccess')
-  // ^ TODO Remove this test code
-
   return (
     <div>
       You have successfully signed up. Once you confirm your email you will be able to sign in.
