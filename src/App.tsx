610380bbf43896fc207876b1a08bc033c8021ec1
Eli Lemons <me@elilemons.com>
2023-10-31 18:05:48 -0400

    Toast container added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { AuthProvider } from '@components/appProviders/Auth'
 import { InAppBrowserListener } from '@components/elements/InAppBrowserListener'
 import { OnRouteChange } from '@components/elements/OnRouteChange'
 import { Routes } from '@root/views/Routes'
+import { Slide, toast, ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const queryClient = new QueryClient({
@@ -20,17 +23,24 @@ function App() {
   })
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className='app'>
-        <Router>
-          <OnRouteChange />
-          <InAppBrowserListener />
-          <AuthProvider>
-            <Routes />
-          </AuthProvider>
-        </Router>
-      </div>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div className='app'>
+          <Router>
+            <OnRouteChange />
+            <InAppBrowserListener />
+            <AuthProvider>
+              <Routes />
+            </AuthProvider>
+          </Router>
+        </div>
+        <ToastContainer
+          position={toast.POSITION.BOTTOM_CENTER}
+          transition={Slide}
+          autoClose={2000}
+        />
+      </QueryClientProvider>
+    </>
   )
 }
 
