487db6e78599cf0f13e7c2676418badfbad7d99c
Eli Lemons <me@elilemons.com>
2023-10-31 17:11:17 -0400

    Some basic top nav stuff

--- src/components/elements/TopNav/index.tsx
+++ src/components/elements/TopNav/index.tsx
@@ -1,25 +1,46 @@
 import { useAuth } from '@components/appProviders/Auth'
+import { APP_ROUTES } from '@root/appRoutes'
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 const TopNav: React.FC = () => {
-  const { user } = useAuth()
-  // TODO Remove this test code
-  console.log('ELITEST TopNav')
-  // ^ TODO Remove this test code
+  const { user, logOut } = useAuth()
 
   return user ? (
     <div>
-      <div>Logo</div>
-      <div>Dashboard</div>
+      <div>
+        <Link to={APP_ROUTES.authenticated.dashboard}>Logo</Link>
+      </div>
+
+      <div>
+        <Link to={APP_ROUTES.authenticated.dashboard}>Dashboard</Link>
+      </div>
+
       <div>Calendar</div>
       <div>Account</div>
+
+      <div>
+        <div style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={logOut}>
+          Logout
+        </div>
+      </div>
     </div>
   ) : (
     <div>
-      <div>Logo</div>
-      <div>Home</div>
-      <div>Login</div>
-      <div>Get Started</div>
+      <div>
+        <Link to={APP_ROUTES.unauthenticated.home}>Logo</Link>
+      </div>
+
+      <div>
+        <Link to={APP_ROUTES.unauthenticated.home}>Home</Link>
+      </div>
+
+      <div>
+        <Link to={APP_ROUTES.unauthenticated.login}>Login</Link>
+      </div>
+      <div>
+        <Link to={APP_ROUTES.unauthenticated.signup}>Sign Up</Link>
+      </div>
     </div>
   )
 }
