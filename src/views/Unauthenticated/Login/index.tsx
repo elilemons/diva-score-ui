6ed3dddea0bc806c81105e059d28c277c0fbe378
Eli Lemons <me@elilemons.com>
2023-11-01 19:58:20 -0400

    Login will prefill email from URL params

--- src/views/Unauthenticated/Login/index.tsx
+++ src/views/Unauthenticated/Login/index.tsx
@@ -3,23 +3,33 @@ import { UserLogin } from '@components/appProviders/Auth/types'
 import { ControlledEmailInput } from '@components/forms/fields/Email/Controlled'
 import { ControlledPasswordInput } from '@components/forms/fields/Password/Controlled'
 import { Submit } from '@components/forms/Submit'
+import { APP_ROUTES } from '@root/appRoutes'
 import * as React from 'react'
 import { useForm } from 'react-hook-form'
+import { Link, useParams } from 'react-router-dom'
 
 const Login: React.FC = () => {
-  const { control, handleSubmit } = useForm<UserLogin>()
+  const { email } = useParams<{ email: string }>()
+  const { control, handleSubmit, watch } = useForm<UserLogin>()
 
   const { logIn } = useAuth()
   const handleLogin = React.useCallback((fieldValues: UserLogin) => logIn(fieldValues), [logIn])
+
+  const emailInput = watch('email')
 
   return (
     <div>
       <h1>Login</h1>
 
       <form onSubmit={handleSubmit(handleLogin)}>
-        <ControlledEmailInput control={control} required name='email' />
+        <ControlledEmailInput control={control} required name='email' defaultValue={email} />
         <ControlledPasswordInput control={control} required name='password' />
         <Submit label='Log In' control={control} />
+        <Link
+          to={`${APP_ROUTES.unauthenticated.forgotPassword}${emailInput ? `/${emailInput}` : ''}`}
+        >
+          Forgot Password?
+        </Link>
       </form>
     </div>
   )
