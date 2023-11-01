6a23fc50d118bb3a289c3a2ec6dc931ccd4ebb8e
Eli Lemons <me@elilemons.com>
2023-11-01 18:25:47 -0400

    Some changes on what is showed on home if auth

--- src/views/Unauthenticated/Home/index.tsx
+++ src/views/Unauthenticated/Home/index.tsx
@@ -1,12 +1,10 @@
+import { useAuth } from '@components/appProviders/Auth'
 import { APP_ROUTES } from '@root/appRoutes'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => {
-  // TODO Remove this test code
-  console.log('ELITEST Home Page')
-  // ^ TODO Remove this test code
-
+  const { user } = useAuth()
   return (
     <div>
       <h2>Daily Inspired Vision and Actions</h2>
@@ -17,9 +15,26 @@ const Home: React.FC = () => {
         and cultivate gratitude and connection with this app.
       </p>
 
-      {/* TODO Make Button Component */}
-      <Link to={APP_ROUTES.unauthenticated.signup}>Get Started</Link>
-      <Link to={APP_ROUTES.unauthenticated.login}>Login</Link>
+      {/* TODO Add Button Component */}
+      {user ? (
+        <>
+          <div>
+            <Link to={APP_ROUTES.authenticated.dashboard}>Dashboard</Link>
+          </div>
+          <div>
+            <Link to={APP_ROUTES.authenticated.account}>Manage Account</Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <Link to={APP_ROUTES.unauthenticated.signup}>Get Started</Link>
+          </div>
+          <div>
+            <Link to={APP_ROUTES.unauthenticated.login}>Login</Link>
+          </div>
+        </>
+      )}
     </div>
   )
 }
