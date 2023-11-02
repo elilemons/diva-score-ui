8cb3b4d5ef2ee3e106ae59ada44b59b020e9fe8a
Eli Lemons <me@elilemons.com>
2023-11-02 16:45:26 -0400

    Added Not Found Page

---
+++ src/views/Global/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const NotFound: React.FC = () => {
+  // TODO Remove this test code
+  console.log('ELITEST NotFound')
+  // ^ TODO Remove this test code
+
+  return <div>404 Page Not Found</div>
+}
+
+export default NotFound
