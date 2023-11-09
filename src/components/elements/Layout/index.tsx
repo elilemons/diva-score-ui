f78aaeb477b43e662fd8f6e9917594f2e15f196a
Eli Lemons <me@elilemons.com>
2023-11-09 14:31:13 -0500

    Layout page is styled and cute

--- src/components/elements/Layout/index.tsx
+++ src/components/elements/Layout/index.tsx
@@ -1,19 +1,59 @@
 import * as React from 'react'
 
-import { Container, Flex } from '@chakra-ui/react'
-import TopNav from '@components/elements/TopNav'
+import { Container, Flex, Stack } from '@chakra-ui/react'
+import { Logo } from '@components/elements/Logo'
+import { APP_CONTAINER_WIDTH, APP_PADDING, APP_SPACING } from '@utils/appStyling'
 
 type Props = {
-  children: React.ReactNode
+  bottomContent: React.ReactNode
+  topContent: React.ReactNode
 }
 
-const Layout: React.FC<Props> = ({ children }: Props) => {
+export const Layout: React.FC<Props> = ({ bottomContent, topContent = true }: Props) => {
   return (
-    <Flex direction='column'>
-      <TopNav />
-      <Container>{children}</Container>
+    <Flex
+      className={'outer-container'}
+      backgroundColor='brand.500'
+      bgGradient='linear(to-br, accent.500, brand.500, brand.700)'
+      height='100%'
+      direction='column'
+      alignItems='center'
+    >
+      <Stack
+        alignContent='center'
+        className='top-content'
+        color='white'
+        justifyContent={'center'}
+        minWidth={APP_CONTAINER_WIDTH.minWidth}
+        px={APP_PADDING.px}
+        pt={APP_PADDING.pt}
+        pb={APP_PADDING.pb}
+        spacing={APP_SPACING.spacing}
+        width={APP_CONTAINER_WIDTH.width}
+      >
+        <Logo />
+        {topContent}
+      </Stack>
+      <Flex
+        className='bottom-container'
+        alignItems='center'
+        height='100%'
+        direction='column'
+        justifyContent={'stretch'}
+        width={{ base: '100%' }}
+      >
+        <Container
+          backgroundColor='white'
+          className='inner-container'
+          height='100%'
+          minWidth={APP_CONTAINER_WIDTH.minWidth}
+          px={APP_PADDING.px}
+          py={APP_PADDING.py}
+          width={APP_CONTAINER_WIDTH.width}
+        >
+          {bottomContent}
+        </Container>
+      </Flex>
     </Flex>
   )
 }
-
-export default Layout
---
+++ src/components/elements/Logo/index.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react'
+import * as React from 'react'
+
+export const Logo: React.FC = () => {
+  // TODO Remove this test code
+  console.log('ELITEST Logo')
+  // ^ TODO Remove this test code
+
+  return <Heading size={{ sm: 'sm', md: 'md' }}>DIVA Score</Heading>
+}
