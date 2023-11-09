3e4d1493a1ecd32ef16b004ef658540d6650c990
Eli Lemons <me@elilemons.com>
2023-11-09 14:31:24 -0500

    Some constants for styling

---
+++ src/utils/appStyling.ts
@@ -0,0 +1,46 @@
+export const APP_PADDING = {
+  px: {
+    base: 8,
+    md: 14,
+  },
+  py: {
+    base: 16,
+    md: 20,
+  },
+  pt: {
+    base: 12,
+    md: 14,
+  },
+  pb: {
+    base: 4,
+    md: 8,
+  },
+}
+
+export const APP_SPACING = {
+  spacing: {
+    base: 2,
+    md: 4,
+  },
+}
+
+export const APP_CONTAINER_WIDTH = {
+  minWidth: {
+    base: '100%',
+    md: '800px',
+  },
+  width: {
+    base: '46%',
+  },
+}
+
+export const APP_PAGE_HEADINGS = {
+  size: { base: 'md', md: 'lg', lg: 'xl', xl: 'xl', '2xl': '2xl' },
+}
+
+export const APP_PAGE_DESCRIPTIONS = {
+  fontSize: {
+    base: 'lg',
+    md: 'xl',
+  },
+}
