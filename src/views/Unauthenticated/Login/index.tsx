c729fd7fa6f4298756c5d102906adef7fd678bcf
Eli Lemons <me@elilemons.com>
2023-11-14 16:54:26 -0500

    Login page styled

--- src/views/Unauthenticated/Login/index.tsx
+++ src/views/Unauthenticated/Login/index.tsx
@@ -1,9 +1,12 @@
+import { Center, Heading, Link as ChakraLink, Stack } from '@chakra-ui/react'
 import { useAuth } from '@components/appProviders/Auth'
 import { UserLogin } from '@components/appProviders/Auth/types'
+import { Layout } from '@components/elements/Layout'
 import { ControlledEmailInput } from '@components/forms/fields/Email/Controlled'
 import { ControlledPasswordInput } from '@components/forms/fields/Password/Controlled'
 import { Submit } from '@components/forms/Submit'
 import { APP_ROUTES } from '@root/appRoutes'
+import { APP_BRAND_BUTTON, APP_FORM_HEADINGS, APP_FORM_STACK_SPACING } from '@utils/appStyling'
 import * as React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link, useParams } from 'react-router-dom'
@@ -18,20 +21,42 @@ const Login: React.FC = () => {
   const emailInput = watch('email')
 
   return (
-    <div>
-      <h1>Login</h1>
+    <Layout
+      bottomContent={
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <Stack spacing={APP_FORM_STACK_SPACING.spacing}>
+            <Heading size={APP_FORM_HEADINGS.size}>Login to DIVA Score</Heading>
 
-      <form onSubmit={handleSubmit(handleLogin)}>
-        <ControlledEmailInput control={control} required name='email' defaultValue={email} />
-        <ControlledPasswordInput control={control} required name='password' />
-        <Submit label='Log In' control={control} />
-        <Link
-          to={`${APP_ROUTES.unauthenticated.forgotPassword}${emailInput ? `/${emailInput}` : ''}`}
-        >
-          Forgot Password?
-        </Link>
-      </form>
-    </div>
+            <ControlledEmailInput
+              control={control}
+              required
+              name='email'
+              label='Email'
+              defaultValue={email}
+            />
+            <ControlledPasswordInput control={control} required name='password' label='Password' />
+
+            <Submit
+              label='Log In'
+              control={control}
+              colorScheme={APP_BRAND_BUTTON.colorScheme}
+              bgGradient={APP_BRAND_BUTTON.bgGradient}
+            />
+            <Center>
+              <ChakraLink
+                color={'brand.500'}
+                as={Link}
+                to={`${APP_ROUTES.unauthenticated.forgotPassword}${
+                  emailInput ? `/${emailInput}` : ''
+                }`}
+              >
+                Forgot Password?
+              </ChakraLink>
+            </Center>
+          </Stack>
+        </form>
+      }
+    />
   )
 }
 
