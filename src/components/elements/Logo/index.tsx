1371f97f474b3005e6046a78e2d924cc0ad52f7b
Eli Lemons <me@elilemons.com>
2023-11-14 17:39:34 -0500

    Logo is a link now

--- src/components/elements/Logo/index.tsx
+++ src/components/elements/Logo/index.tsx
@@ -1,10 +1,16 @@
 import { Heading } from '@chakra-ui/react'
+import { APP_ROUTES } from '@root/appRoutes'
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Logo: React.FC = () => {
   // TODO Remove this test code
   console.log('ELITEST Logo')
   // ^ TODO Remove this test code
 
-  return <Heading size={{ sm: 'sm', md: 'md' }}>DIVA Score</Heading>
+  return (
+    <Heading size={{ sm: 'sm', md: 'md' }}>
+      <Link to={APP_ROUTES.global.home}>DIVA Score</Link>
+    </Heading>
+  )
 }
