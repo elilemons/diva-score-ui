f053f542e47a5fe1a5369cf18dd97fbae0669071
Eli Lemons <me@elilemons.com>
2023-11-28 18:32:53 -0500

    Some notes for myself

--- src/views/Authenticated/Dashboard/index.tsx
+++ src/views/Authenticated/Dashboard/index.tsx
@@ -1,11 +1,13 @@
-import { Heading, Stack } from '@chakra-ui/react'
+import { Button, Heading, Stack } from '@chakra-ui/react'
 import { useAuth } from '@components/appProviders/Auth'
 import { Layout } from '@components/elements/Layout'
-import { APP_INNER_HEADINGS, APP_SPACING } from '@utils/appStyling'
+import { APP_BRAND_BUTTON, APP_INNER_HEADINGS, APP_SPACING } from '@utils/appStyling'
 import * as React from 'react'
 
 const Dashboard: React.FC = () => {
   const { user } = useAuth()
+
+  // TODO On begin survey click, we create a survey than navigate to that survey page to edit it
 
   return (
     <Layout
@@ -14,6 +16,13 @@ const Dashboard: React.FC = () => {
           <Heading data-cy='welcome-message' size={APP_INNER_HEADINGS.size} textAlign='center'>
             Welcome to your Dashboard {user && `${user.firstName}`}!
           </Heading>
+          <Button
+            data-cy='beginDailySurvey'
+            colorScheme={APP_BRAND_BUTTON.colorScheme}
+            bgGradient={APP_BRAND_BUTTON.bgGradient}
+          >
+            Begin Today's Entry
+          </Button>
         </Stack>
       }
     />
