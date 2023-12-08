161378a017e04c271b9b870e4609629be94df438
Eli Lemons <me@elilemons.com>
2023-12-07 19:29:17 -0500

    Logic to handle existing survey added

--- src/views/Authenticated/Dashboard/index.tsx
+++ src/views/Authenticated/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import { useAuth } from '@components/appProviders/Auth'
 import { Layout } from '@components/elements/Layout'
 import { APP_ROUTES } from '@root/appRoutes'
 import { createSurveyMutation } from '@root/queries/survey/createSurveyMutation'
+import { getTodaysSurveyQuery } from '@root/queries/survey/getTodaysSurvey'
 import { GenericStatusError, GenericStatusErrorType } from '@root/types/errors'
 import { toastErrors } from '@root/utils/toastErrors'
 import { APP_BRAND_BUTTON, APP_INNER_HEADINGS, APP_SPACING } from '@utils/appStyling'
@@ -12,39 +13,44 @@ import { useHistory } from 'react-router-dom'
 const Dashboard: React.FC = () => {
   const { user } = useAuth()
   const history = useHistory()
+  const { data: existingSurvey } = getTodaysSurveyQuery()
   const createSurvey = createSurveyMutation({ mutationKey: 'user-create-daily-survey' })
 
   const onBeginClick = async () => {
-    try {
-      if (user) {
-        await createSurvey
-          .mutateAsync({
-            title: 'Daily Check-in',
-            surveyUser: user?.id,
-            surveyDate: new Date(),
-          })
-          .then(res => {
-            if (res.doc) {
-              history.push(`${APP_ROUTES.authenticated.survey}/${res.doc.id}`)
-            } else {
-              throw GenericStatusError({
-                status: 501,
-                message: 'An unknown error occured while creating the survey.',
-              })
-            }
-          })
-          .catch(error => {
-            throw error
-          })
+    if (existingSurvey && existingSurvey.id) {
+      history.push(`${APP_ROUTES.authenticated.survey}/${existingSurvey.id}`)
+    } else {
+      try {
+        if (user) {
+          await createSurvey
+            .mutateAsync({
+              title: 'Daily Check-in',
+              surveyUser: user?.id,
+              surveyDate: new Date(),
+            })
+            .then(res => {
+              if (res.doc) {
+                history.push(`${APP_ROUTES.authenticated.survey}/${res.doc.id}`)
+              } else {
+                throw GenericStatusError({
+                  status: 501,
+                  message: 'An unknown error occured while creating the survey.',
+                })
+              }
+            })
+            .catch(error => {
+              throw error
+            })
+        }
+      } catch (e) {
+        const error = e as GenericStatusErrorType
+        toastErrors({
+          error,
+          id: 'create-survey-error',
+          title: 'Survey Creation Error',
+          description: 'There was an error creating the survey, please try again.',
+        })
       }
-    } catch (e) {
-      const error = e as GenericStatusErrorType
-      toastErrors({
-        error,
-        id: 'create-survey-error',
-        title: 'Survey Creation Error',
-        description: 'There was an error creating the survey, please try again.',
-      })
     }
   }
 
@@ -61,7 +67,7 @@ const Dashboard: React.FC = () => {
             bgGradient={APP_BRAND_BUTTON.bgGradient}
             onClick={onBeginClick}
           >
-            Begin Today's Entry
+            {existingSurvey && existingSurvey.id ? 'Continue' : 'Begin'} Today's Entry
           </Button>
         </Stack>
       }
