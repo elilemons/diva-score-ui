2a6e26bdee2712cf5088138d24a2a775b64a6a4f
Eli Lemons <me@elilemons.com>
2023-12-18 19:34:09 -0500

    Loading has a layout now

--- src/components/elements/FullscreenLoader/index.tsx
+++ src/components/elements/FullscreenLoader/index.tsx
@@ -1,7 +1,15 @@
+import { Heading } from '@chakra-ui/react'
+import { Layout } from '@root/components/elements/Layout'
 import * as React from 'react'
 
 const FullscreenLoader: React.FC = () => {
-  return <div data-cy='loading'>Loading...</div>
+  return (
+    <Layout
+      isLoaded={false}
+      topContent={<Heading data-cy='loading'>Loading...</Heading>}
+      bottomContent={<></>}
+    />
+  )
 }
 
 export default FullscreenLoader
