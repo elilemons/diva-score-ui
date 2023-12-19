b21f774b8c1c29818d243c737409135b692de45f
Eli Lemons <me@elilemons.com>
2023-12-18 19:34:18 -0500

    Full screen that loader

--- src/components/elements/FullscreenLoader/index.tsx
+++ src/components/elements/FullscreenLoader/index.tsx
@@ -1,13 +1,23 @@
-import { Heading } from '@chakra-ui/react'
+import { Flex, Spinner } from '@chakra-ui/react'
 import { Layout } from '@root/components/elements/Layout'
 import * as React from 'react'
 
 const FullscreenLoader: React.FC = () => {
   return (
     <Layout
-      isLoaded={false}
-      topContent={<Heading data-cy='loading'>Loading...</Heading>}
-      bottomContent={<></>}
+      bottomContent={
+        <Flex
+          position='fixed'
+          top={0}
+          right={0}
+          bottom={0}
+          left={0}
+          alignItems='center'
+          justifyContent='center'
+        >
+          <Spinner color='brand.300' />
+        </Flex>
+      }
     />
   )
 }
