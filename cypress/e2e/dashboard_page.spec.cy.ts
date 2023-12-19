3cb72fa46f222dff1614e42e7a416f6eb26c90ce
Eli Lemons <me@elilemons.com>
2023-12-18 19:13:27 -0500

    Added some async stuff to this test

--- cypress/e2e/dashboard_page.spec.cy.ts
+++ cypress/e2e/dashboard_page.spec.cy.ts
@@ -4,6 +4,16 @@ describe('The Dashboard Page', () => {
   beforeEach(() => {
     cy.loginViaUI() // Server must be running for this to work
     cy.visit('/dashboard')
+
+    // Make sure the page isn't loading
+    cy.get("[data-cy='loading']").should('not.exist')
+
+    // Intercept the network request for 'get-todays-survey' endpoint
+    cy.intercept('GET', '/api/surveys/get-todays-survey').as('getTodaysSurvey')
+
+    // Wait for the 'get-todays-survey' request to finish
+    cy.wait('@getTodaysSurvey')
+
     cy.injectAxe()
   })
 
