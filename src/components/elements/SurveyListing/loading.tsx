f7353db13d55cb2583d32de18bd59f045670de6d
Eli Lemons <me@elilemons.com>
2024-03-04 00:20:32 -0500

    Prettier skeletons and better spacing

--- src/components/elements/SurveyListing/loading.tsx
+++ src/components/elements/SurveyListing/loading.tsx
@@ -2,15 +2,25 @@ import { Badge, Box, Flex, Skeleton, SkeletonCircle, SkeletonText } from '@chakr
 import { APP_PADDING } from '@root/utils/appStyling'
 import * as React from 'react'
 
-export const SurveyListingSkeleton: React.FC = () => {
+type Props = {
+  backgroundColor?: string
+}
+
+export const SurveyListingSkeleton: React.FC<Props> = ({ backgroundColor = 'white' }) => {
   return (
-    <Box px={APP_PADDING.px} data-cy='survey-listing-skeleton'>
+    <Box
+      height='75'
+      pt={1}
+      px={APP_PADDING.px}
+      data-cy='survey-listing-skeleton'
+      bgColor={backgroundColor}
+    >
       <Flex alignItems='center' justifyContent='space-between' width='100% '>
         <Flex alignItems='center' py={1} gap={4}>
           <SkeletonCircle size='50' />
           <SkeletonText noOfLines={2} skeletonHeight='2' width='115px' />
         </Flex>
-        <Skeleton>
+        <Skeleton startColor='accent.100'>
           <Badge>+00</Badge>
         </Skeleton>
       </Flex>
