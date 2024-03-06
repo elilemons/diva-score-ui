dc7c712d4f98101463eeb60cd4761eefe4141503
Eli Lemons <me@elilemons.com>
2024-03-06 18:58:50 -0500

    Components for terms

---
+++ src/views/Global/PrivacyPolicy/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const PrivacyPolicy: React.FC = () => {
+  // TODO Remove this test code
+  console.log('ELITEST PrivacyPolicy')
+  // ^ TODO Remove this test code
+
+  return <div></div>
+}
+
+export default PrivacyPolicy
---
+++ src/views/Global/TermsOfService/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const TermsOfService: React.FC = () => {
+  // TODO Remove this test code
+  console.log('ELITEST TermsOfService')
+  // ^ TODO Remove this test code
+
+  return (
+    <div>
+      <iframe src='./index.html' title='Terms of Service' width='100%' height='100v'></iframe>
+    </div>
+  )
+}
+
+export default TermsOfService
