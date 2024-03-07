b8f64c72c7fc31ed564a5bd18d1c4b480d39800c
Eli Lemons <me@elilemons.com>
2024-03-07 00:18:30 -0500

    Logo

--- src/components/elements/Logo/index.tsx
+++ src/components/elements/Logo/index.tsx
@@ -1,12 +1,20 @@
-import { Heading } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
 import { APP_ROUTES } from '@root/appRoutes'
+import { APP_PADDING } from '@root/utils/appStyling'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Logo: React.FC = () => {
   return (
-    <Heading size={{ sm: 'sm', md: 'md' }}>
-      <Link to={APP_ROUTES.global.home}>DIVA Score</Link>
-    </Heading>
+    <Box py={APP_PADDING.pb}>
+      <Link to={APP_ROUTES.global.home}>
+        <Image
+          src='/logo.png'
+          alt='Divinely Inspired Vision and Actions'
+          height='75px'
+          filter='drop-shadow(1px 1px 1px #222)'
+        />
+      </Link>
+    </Box>
   )
 }
