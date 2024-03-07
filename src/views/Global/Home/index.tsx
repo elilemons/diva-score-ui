c20f473b45c32d4205b283c5622e19d1ce6e1b7a
Eli Lemons <me@elilemons.com>
2024-03-07 00:19:44 -0500

    Added some padding bottom

--- src/views/Global/Home/index.tsx
+++ src/views/Global/Home/index.tsx
@@ -2,7 +2,12 @@ import { Button, Heading, Stack, Text } from '@chakra-ui/react'
 import { useAuth } from '@components/appProviders/Auth'
 import { Layout } from '@components/elements/Layout'
 import { APP_ROUTES } from '@root/appRoutes'
-import { APP_PAGE_DESCRIPTIONS, APP_PAGE_HEADINGS, APP_SPACING } from '@utils/appStyling'
+import {
+  APP_PADDING,
+  APP_PAGE_DESCRIPTIONS,
+  APP_PAGE_HEADINGS,
+  APP_SPACING,
+} from '@utils/appStyling'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -16,7 +21,7 @@ const Home: React.FC = () => {
           <Heading as='h1' size={APP_PAGE_HEADINGS.size} ml={APP_PAGE_HEADINGS.ml}>
             Divinely Inspired Vision and Actions
           </Heading>
-          <Text fontSize={APP_PAGE_DESCRIPTIONS.fontSize}>
+          <Text fontSize={APP_PAGE_DESCRIPTIONS.fontSize} pb={APP_PADDING.pb}>
             Elevate the self-care journey for Divinely Inspired Vision and Action with your own DIVA
             Score. Daily check-ins on body, mind, and spirit activities, each with point values,
             provide a personalized Divine Self-Care Score. Stay accountable to your well-being and
