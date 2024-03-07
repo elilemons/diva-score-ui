33f9ddbdbc1b032faa9adc2b730df82f847212c3
Eli Lemons <me@elilemons.com>
2024-03-07 00:19:11 -0500

    Removed unused test

--- src/views/Global/Home/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@utils/testUtils'
-import Home from './'
-
-test('renders page heading', async () => {
-  render(<Home />)
-  const headingElement = await screen.findByText(/Divinely Inspired Vision and Actions/)
-  expect(headingElement).toBeInTheDocument()
-})
