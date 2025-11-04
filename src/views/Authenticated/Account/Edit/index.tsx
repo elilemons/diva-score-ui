13c6306c7974b2e24abf8573c94768085d69f8f2
Eli Lemons <me@elilemons.com>
2025-11-04 14:30:05 -0500

    Edit account working

--- src/views/Authenticated/Account/Edit/index.tsx
+++ src/views/Authenticated/Account/Edit/index.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from '@components/appProviders/Auth'
-import { FC, useCallback, useEffect, useState } from 'react'
+import { FC, useCallback } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { Stack, useToast } from '@chakra-ui/react'
@@ -8,52 +8,38 @@ import { ControlledEmailInput } from '@components/forms/fields/Email/Controlled'
 import { Submit } from '@components/forms/Submit'
 import { User } from '@elilemons/diva-score-lib'
 import { ControlledTextInput } from '@root/components/forms/fields/Text/Controlled'
-import { getForgotPasswordTokenQuery } from '@root/queries/user/getForgotPasswordToken'
 import { meMutation } from '@root/queries/user/meMutation'
-import { userResetPasswordMutation } from '@root/queries/user/userResetPasswordMutation'
 import { GenericStatusErrorType } from '@root/types/errors'
 import { APP_BRAND_BUTTON, APP_SPACING } from '@utils/appStyling'
 import { toastErrors } from '@utils/toastErrors'
-import { useHistory } from 'react-router-dom'
 import { AccountLayout } from '../Layout'
 
 const AccountEdit: FC = () => {
-  const { user, logOut } = useAuth()
+  const { user } = useAuth()
   const { control, handleSubmit } = useForm<Partial<User>>()
-  const resetPassword = userResetPasswordMutation()
-  const [passwordChanging, setPasswordChanging] = useState(false)
-  const [resetPasswordToken, setResetPasswordToken] = useState<string | undefined>()
-  const [newPassword, setNewPassword] = useState<string | undefined>()
 
   const updateMe = meMutation({ mutationKey: 'account-page-update' })
 
-  const history = useHistory()
   const toast = useToast()
-
-  const { data: tokenData } = getForgotPasswordTokenQuery({ run: passwordChanging })
 
   const onSubmit = useCallback(
     async (data: Partial<User>) => {
       try {
-        if (user && data.email !== user.email) {
-          // Update email
-          await updateMe.mutateAsync({
-            data,
-          })
-
-          const toastId = 'account-update-success'
-          if (!toast.isActive(toastId)) {
-            toast({
-              id: toastId,
-              description: 'Your account has been updated.',
-              status: 'success',
-            })
-          }
+        if (!user) {
+          return
         }
 
-        if (user && data.password) {
-          setPasswordChanging(true)
-          setNewPassword(data.password)
+        await updateMe.mutateAsync({
+          data,
+        })
+
+        const toastId = 'account-update-success'
+        if (!toast.isActive(toastId)) {
+          toast({
+            id: toastId,
+            description: 'Your account has been updated.',
+            status: 'success',
+          })
         }
       } catch (e) {
         const error = e as GenericStatusErrorType
@@ -67,42 +53,6 @@ const AccountEdit: FC = () => {
     },
     [user],
   )
-
-  useEffect(() => {
-    if (tokenData && passwordChanging) {
-      setResetPasswordToken(tokenData.token)
-    }
-  }, [tokenData, passwordChanging])
-
-  useEffect(() => {
-    if (resetPasswordToken && newPassword) {
-      try {
-        resetPassword.mutateAsync({ token: resetPasswordToken, password: newPassword }).then(() => {
-          const toastId = 'reset-password-success'
-          if (!toast.isActive(toastId)) {
-            toast({
-              id: toastId,
-              title: 'Reset Password Success',
-              description:
-                'Your password has been changed! Now please login again with your new password.',
-              status: 'success',
-            })
-          }
-
-          history.push(`/login`)
-          logOut()
-        })
-      } catch (e) {
-        const error = e as GenericStatusErrorType
-        toastErrors({
-          error,
-          id: 'reset-password-errors',
-          title: 'Reset Password Failure',
-          description: 'There was an error creating your account, please try again.',
-        })
-      }
-    }
-  }, [resetPasswordToken, newPassword])
 
   return (
     <Layout
